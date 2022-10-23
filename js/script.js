1179c1e2e45e6bccd8aa92d42147ce7854128f6a
Marek Slowinski <marekslowinski@gmail.com>
2022-10-23 17:17:23 +0200

    function added

--- js/script.js
+++ js/script.js
@@ -1,39 +1,80 @@
-console.log("Hej, tu Marek Słowiński :) Witaj na kursie Frontend Developer");
+{
+  const welcome = () => {
+    console.log(
+      "Hej, tu Marek Słowiński :) Witaj na kursie Frontend Developer"
+    );
+  };
 
-let buttonElement = document.querySelector(".js-button");
-let headerElement = document.querySelector(".header");
-let navigationElement = document.querySelector(".navigation");
-let headerElements = document.querySelectorAll(".article__header");
-let subHeaderElements = document.querySelectorAll(".article__subHeader");
-let changeBodyElement = document.querySelector(".body");
-let changeFooterElement = document.querySelector(".footerContainer");
+  const buttonElement = document.querySelector(".js-button");
 
-buttonElement.addEventListener("click", () => {
-  if (buttonElement.innerText === "Change to dark") {
-    buttonElement.innerText = "Change to bright";
-    buttonElement.classList.toggle("changeButton");
-    navigationElement.classList.toggle("changeNavigation");
-    changeBodyElement.classList.toggle("changeBody");
-    headerElement.classList.toggle("changeAllHeaders");
+  const buttonElementChange = () => {
+    if (buttonElement.innerText === "Change to dark") {
+      buttonElement.innerText = "Change to bright";
+      buttonElementToggle();
+      changeNavigationToggle();
+      changeBodyElementToggle();
+      headerElementsToggle();
+      changeFooter();
+      headerElementToggle();
+      subHeaderElementsToggle();
+    } else if (buttonElement.innerText === "Change to bright") {
+      buttonElement.innerText = "Change to dark";
+      buttonElementToggle();
+      changeNavigationToggle();
+      changeBodyElementToggle();
+      headerElementsToggle();
+      changeFooter();
+      headerElementToggle();
+      subHeaderElementsToggle();
+    }
+  };
+
+  const buttonElementToggle = () => {
+    return buttonElement.classList.toggle("changeButton");
+  };
+
+  const changeNavigationToggle = () => {
+    const navigationElement = document.querySelector(".navigation");
+    return navigationElement.classList.toggle("changeNavigation");
+  };
+
+  const changeBodyElementToggle = () => {
+    const changeBodyElement = document.querySelector(".body");
+    return changeBodyElement.classList.toggle("changeBody");
+  };
+
+  const headerElementToggle = () => {
+    const headerElement = document.querySelector(".header");
+    return headerElement.classList.toggle("changeAllHeaders");
+  };
+
+  const changeFooter = () => {
+    const changeFooterElement = document.querySelector(".footerContainer");
     changeFooterElement.classList.toggle("changeFooter");
+  };
+
+  const headerElementsToggle = () => {
+    const headerElements = document.querySelectorAll(".article__header");
     headerElements.forEach((Element) => {
       Element.classList.toggle("changeAllHeaders");
     });
+  };
+
+  const subHeaderElementsToggle = () => {
+    const subHeaderElements = document.querySelectorAll(".article__subHeader");
     subHeaderElements.forEach((Element) => {
       Element.classList.toggle("changeAllHeaders");
     });
-  } else if (buttonElement.innerText === "Change to bright") {
-    buttonElement.innerText = "Change to dark";
-    buttonElement.classList.toggle("changeButton");
-    navigationElement.classList.toggle("changeNavigation");
-    headerElement.classList.toggle("changeAllHeaders");
-    changeBodyElement.classList.toggle("changeBody");
-    changeFooterElement.classList.toggle("changeFooter");
-    headerElements.forEach((Element) => {
-      Element.classList.toggle("changeAllHeaders");
-    });
-    subHeaderElements.forEach((Element) => {
-      Element.classList.toggle("changeAllHeaders");
-    });
-  }
-});
+  };
+
+  const onButtonClick = () => {
+    buttonElementChange();
+  };
+
+  const init = () => {
+    welcome();
+    buttonElement.addEventListener("click", onButtonClick);
+  };
+
+  init();
+}
