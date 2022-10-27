0c64d8574aedf40add28377d68561966f77dd141
Marek Slowinski <marekslowinski@gmail.com>
2022-10-27 10:40:04 +0200

    buttonElement as function parameter added

--- js/script.js
+++ js/script.js
@@ -5,13 +5,11 @@
     );
   };
 
-  const buttonElement = document.querySelector(".js-button");
-
-  const buttonElementChange = () => {
+  const buttonElementChange = (buttonElement) => {
     switch (buttonElement.innerText) {
       case "Change to dark":
         buttonElement.innerText = "Change to bright";
-        buttonElementToggle();
+        buttonElementToggle(buttonElement);
         changeNavigationToggle();
         changeBodyElementToggle();
         headerElementsToggle();
@@ -21,7 +19,7 @@
         break;
       case "Change to bright":
         buttonElement.innerText = "Change to dark";
-        buttonElementToggle();
+        buttonElementToggle(buttonElement);
         changeNavigationToggle();
         changeBodyElementToggle();
         headerElementsToggle();
@@ -32,32 +30,37 @@
     }
   };
 
-  const buttonElementToggle = () => {
+  const buttonElementToggle = (buttonElement) => {
     return buttonElement.classList.toggle("changeButton");
   };
 
   const changeNavigationToggle = () => {
     const navigationElement = document.querySelector(".navigation");
+
     return navigationElement.classList.toggle("changeNavigation");
   };
 
   const changeBodyElementToggle = () => {
     const changeBodyElement = document.querySelector(".body");
+
     return changeBodyElement.classList.toggle("changeBody");
   };
 
   const headerElementToggle = () => {
     const headerElement = document.querySelector(".header");
+
     return headerElement.classList.toggle("changeAllHeaders");
   };
 
   const changeFooter = () => {
     const changeFooterElement = document.querySelector(".footerContainer");
+
     changeFooterElement.classList.toggle("changeFooter");
   };
 
   const headerElementsToggle = () => {
     const headerElements = document.querySelectorAll(".article__header");
+
     headerElements.forEach((Element) => {
       Element.classList.toggle("changeAllHeaders");
     });
@@ -71,10 +74,13 @@
   };
 
   const onButtonClick = () => {
-    buttonElementChange();
+    const buttonElement = document.querySelector(".js-button");
+    buttonElementChange(buttonElement);
   };
 
   const init = () => {
+    const buttonElement = document.querySelector(".js-button");
+
     welcome();
     buttonElement.addEventListener("click", onButtonClick);
   };
